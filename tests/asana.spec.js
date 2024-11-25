79768be3b23a2ad0aea82644884408465907569e
Timur <timka123454321timka@gmail.com>
2024-11-25 10:45:45 -0500

    added under column verification

--- tests/asana.spec.js
+++ tests/asana.spec.js
@@ -15,6 +15,7 @@ test.beforeEach('Test Case 1', async ({ page }) => {
 
 test('Test 1', async ({ page }) => {
   await page.getByLabel('Cross-functional project plan').click();
+  await expect(page.locator("//div[@class='CommentOnlyBoardColumn-header']//h3[contains(text(),'To')][contains(text(),'do')]//following::span[text()='Draft project brief']")).toBeVisible();
   await expect(page.getByText('Non-Priority')).toBeVisible();
   await expect(page.getByText('On track').nth(1)).toBeVisible();
 
@@ -22,6 +23,7 @@ test('Test 1', async ({ page }) => {
 
 test('Test 2', async ({ page }) => {
   await page.getByLabel('Cross-functional project plan').click();
+  await expect(page.locator("//div[@class='CommentOnlyBoardColumn-header']//h3[contains(text(),'To')][contains(text(),'do')]//following::span[contains(text(),'Schedule kickoff meeting']")).toBeVisible();
   await expect(page.getByText('Medium')).toBeVisible();
   await expect(page.getByText('At risk').first()).toBeVisible();
   
@@ -29,14 +31,16 @@ test('Test 2', async ({ page }) => {
 
 test('Test 3', async ({ page }) => {
     await page.getByLabel('Cross-functional project plan').click();
+    await expect(page.locator("//div[@class='CommentOnlyBoardColumn-header']//h3[contains(text(),'To')][contains(text(),'do')]//following::span[text()='Share timeline with teammates']")).toBeVisible();
     await expect(page.getByText('High')).toBeVisible();
-    //  does not have off track
+    //  does not have off track tag on this page, instead only on track
     //await expect(page.getByText('Off track')).toBeVisible();
 
 });
 
 test('Test 4', async ({ page }) => {
     await page.getByLabel('Work Requests, Project').click();
+    await expect(page.locator("//div[@class='CommentOnlyBoardColumn-header']//h3[contains(text(),'New')][contains(text(),'Requests')]//following::span[text()='[Example] Laptop setup for new hire']")).toBeVisible();
     await expect(page.getByText('Medium priority')).toBeVisible();
     await expect(page.getByText('Low effort').first()).toBeVisible();
     await expect(page.getByText('New hardware').first()).toBeVisible();
@@ -45,6 +49,7 @@ test('Test 4', async ({ page }) => {
 
 test('Test 5', async ({ page }) => {
       await page.getByLabel('Work Requests, Project').click();
+      await expect(page.locator("//div[@class='CommentOnlyBoardColumn-header']//h3[contains(text(),'In')][contains(text(),'Progress')]//following::span[text()='[Example] Password not working']")).toBeVisible();
       await expect(page.getByText('Low effort').first()).toBeVisible();
       await expect(page.getByText('Low priority').first()).toBeVisible();
       await expect(page.getByText('Password reset').first()).toBeVisible();
@@ -53,6 +58,7 @@ test('Test 5', async ({ page }) => {
 
 test('Test 6', async ({ page }) => {
       await page.getByLabel('Work Requests, Project').click();
+      await expect(page.locator("//div[@class='CommentOnlyBoardColumn-header']//h3[(text()='Completed')]//following::span[text()='[Example] New keycard for Daniela V']")).toBeVisible();
       await expect(page.getByText('Low effort').first()).toBeVisible();
       await expect(page.getByText('New hardware').first()).toBeVisible();
       await expect(page.getByText('High Priority').first()).toBeVisible();
